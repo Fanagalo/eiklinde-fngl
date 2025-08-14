0e582cd6941cd6aa56f8681b222f1173e7eca355
Jaap Wiering <github.com@fanagalo.nl>
2025-08-14 16:34:48 +0200

    removed Tribe code from functions.php

--- functions.php
+++ functions.php
@@ -197,22 +197,4 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';  // Load Jetpack compatibility file.
 }
 
-
-// remove The Event Calendar (Pro) CSS
-
-add_action( 'wp_enqueue_scripts', 'deregister_tribe_styles' );
- 
-function deregister_tribe_styles() {
-    wp_deregister_style( 'tribe-events-pro-views-v2-skeleton' );
-    wp_deregister_style( 'tribe-events-pro-views-v2-full' );
-    wp_deregister_style( 'tribe-events-views-v2-skeleton' );
-    wp_deregister_style( 'tribe-events-views-v2-full' );
-    wp_deregister_style( 'tribe-common-skeleton-style' );
-    wp_deregister_style( 'tribe-common-full-style' );
-}
-
-
-
-
-
 ?>
